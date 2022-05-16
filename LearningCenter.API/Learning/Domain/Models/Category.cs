6bd4a1e479212f135e9c026c5369312f02e9cc06
Raul Ventura <u201714049@upc.edu.pe>
2022-05-16 18:53:40 -0500

    feat(Categories): Implemented Property-based relationship with Tutorials.

--- LearningCenter.API/Learning/Domain/Models/Category.cs
+++ LearningCenter.API/Learning/Domain/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace LearningCenter.API.Learning.Domain.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LearningCenter.API.Learning.Domain.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public IList<Tutorial> Tutorials { get; set; } = new List<Tutorial>();
     }
 }
