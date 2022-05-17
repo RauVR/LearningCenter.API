a65d3c4fa82c9aa7e0cdca4e2494ce9536dcd74c
Raul Ventura <u201714049@upc.edu.pe>
2022-05-16 22:11:49 -0500

    feat(Category): Implemented Category Service Interface, and Category Resource DTO.

---
+++ LearningCenter.API/Learning/Domain/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+        Task<CategoryResponse> SaveAsync(Category category);
+        Task<CategoryResponse> UpdateAsync(int id, Category category);
+        Task<CategoryResponse> DeleteAsync(int id);
+    }
+}
---
+++ LearningCenter.API/Learning/Resources/CategoryResource.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LearningCenter.API.Learning.Resources
+{
+    public class CategoryResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
