e6359d53e65049523734fd1acf9a794221fac746
Raul Ventura <u201714049@upc.edu.pe>
2022-05-16 20:20:13 -0500

    feat(Category): Implemented Category Response

---
+++ LearningCenter.API/Learning/Domain/Services/Communication/CategoryResponse.cs
@@ -0,0 +1,16 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Shared.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Domain.Services.Communication
+{
+    public class CategoryResponse:BaseResponse<Category>
+    {
+        public CategoryResponse(Category resource) : base(resource)
+        {
+        }
+
+        public CategoryResponse(string message) : base(message)
+        {
+        }
+    }
+}
