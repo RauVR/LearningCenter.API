bf0d7ae40f7d884b1d1867e518fcd8ec1e785f70
Raul Ventura <u201714049@upc.edu.pe>
2022-05-17 19:40:24 -0500

    feat(Category): Implemented Get All Method for Categories Endpoint.

---
+++ LearningCenter.API/Learning/Controllers/CategoriesControllers.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers
+{   //Por buenas practicas el nombre de esta clase debe ser plural
+    //ControllerBase es una clase del framework que esta ya integrado
+    
+    [Route("/api/v1/[controller]")]
+    public class CategoriesControllers : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesControllers(ICategoryService categoryService, IMapper mapper)
+        {   //Esto es inyeccion por constructor
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CategoryResource>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
+
+            return resources;
+        }
+    }
+}
