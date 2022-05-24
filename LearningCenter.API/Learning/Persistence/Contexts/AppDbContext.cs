2d038f46b27486ab9f6a21dd8a74e31da4088ba2
Raul Ventura <u201714049@upc.edu.pe>
2022-05-24 08:25:02 -0500

    feat(Tutorials): Added Persistence constrains and relationships.

--- LearningCenter.API/Learning/Persistence/Contexts/AppDbContext.cs
+++ LearningCenter.API/Learning/Persistence/Contexts/AppDbContext.cs
@@ -11,14 +11,28 @@ namespace LearningCenter.API.Learning.Persistence.Contexts
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Tutorial> Tutorials { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            //Categories
             builder.Entity<Category>().ToTable("Categories");
             builder.Entity<Category>().HasKey(p => p.Id);
             builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
             builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+            
+            //Relationships  esto representa la relacion de uno a muchos
+            builder.Entity<Category>().HasMany(p => p.Tutorials)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+            
+            //Tutorials
+            builder.Entity<Tutorial>().ToTable("Categories");
+            builder.Entity<Tutorial>().HasKey(p => p.Id);
+            builder.Entity<Tutorial>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Tutorial>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+            builder.Entity<Tutorial>().Property(p => p.Description).HasMaxLength(120);
             
             //Apply Naming Convention
             
