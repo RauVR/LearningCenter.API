74edf05252dee88a953481549d0cdcaa8574a2c6
Raul Ventura <u201714049@upc.edu.pe>
2022-05-24 08:35:50 -0500

    feat(tutorials): Added Repository contract.

--- LearningCenter.API/Learning/Domain/Repositories/ITutorialRepository.cs
+++ LearningCenter.API/Learning/Domain/Repositories/ITutorialRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace LearningCenter.API.Learning.Domain.Repositories
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LearningCenter.API.Learning.Domain.Models;
+
+namespace LearningCenter.API.Learning.Domain.Repositories
 {
     public interface ITutorialRepository
     {
-        
+        Task<IEnumerable<Tutorial>> ListAsync();
+        Task AddAsync(Tutorial tutorial);
+        Task<Tutorial> FindByIdAsync(int id);
+        Task<Tutorial> FindByNameAsync(string name);
+        Task<IEnumerable<Tutorial>> FindByCategoryId(int categoryId);
+        void Update(Tutorial tutorial);
+        void Remove(Tutorial tutorial);
     }
 }
